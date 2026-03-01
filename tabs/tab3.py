d0ec572083d17abe54083a4b0e3b517d5b9de9fd
gcbenito1-blip <gc.benito1997@gmail.com>
2026-03-01 11:46:18 +0800

    Update tab3.py

--- tabs/tab3.py
+++ tabs/tab3.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+def render():
+    st.header('Model Evaluation', anchor=False)
+    st.text('Evaluating the accuracy and reliability of the predictive model')
+    with st.container(width=500, border=True):
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("hello")
+        with col2:
+            st.button("button")
